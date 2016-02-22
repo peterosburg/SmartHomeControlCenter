f4e42698f2b0d41db155ea48817a59c5b4b8d3e4
Peter Osburg <peter.osburg+github@gmail.com>
2016-02-22 14:44:30 +0100

    google distance matrix calculation triggered on demand
    moved the call to the onclick event of the respective navigation

--- scripts/mainController.js
+++ scripts/mainController.js
@@ -6,15 +6,20 @@ var mainController = {
 	// call this once and everthing else should be loaded automatically
 	startApplication : function() {
 		// provide true if you have a configuration_custom.json created
-		mainController.getConfiguration(true);		
-		
+		mainController.getConfiguration(true);			
 	},
 
 	// started right after configuration data has been read successfully
-	loadContentData : function() {		
+	loadContentData : function() {
+		// first load the locales, as a callback, continue loading the weather data
 		mainController.loadLocalScript('weatherController.js', mainController.loadWeatherData);	
+		// come on show the time, but don't do it before the locales are loaded
 		mainController.displayDateAndTime();
+		// load the Google Maps API and afterwards add the controller file 
+		// it needs to be done in the callback, to ensure that the API initialization - which is asynchronous - was successful
 		mainController.loadGoogleMapsAPI(mainController.loadGoogleMapsController);
+		// add onclick handlers
+		mainController.configureOnClickHandlers();
 	},
 
 	loadGoogleMapsAPI : function(successCallback) {
@@ -61,12 +66,14 @@ var mainController = {
 	},
 
 	loadGoogleMapsController : function() {
-		mainController.loadLocalScript('googleMapsController.js', mainController.loadTimesOfDistances);
+		// dynamically load the controller class
+		mainController.loadLocalScript('googleMapsController.js');
+		//, mainController.loadTimesOfDistances
 	},
 
-	loadTimesOfDistances : function() {
+	/*loadTimesOfDistances : function() {
 		googleMapsController.loadTimesOfDistances();
-	},
+	},*/
 
 	displayDateAndTime : function() {		
 		var date = Date.now();
@@ -104,6 +111,13 @@ var mainController = {
 			mainController.log('Failed to load local script ' + scriptName);
 			mainController.log(exception);
 		})
+	},
+
+	configureOnClickHandlers : function() {
+		// information button shall trigger reading the time values for origin to destination travels
+		$('#nav_information').click(function(){
+			googleMapsController.loadTimesOfDistances();
+		});
 	}
 
 }
